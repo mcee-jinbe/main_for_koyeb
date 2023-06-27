a82baf55c58add6bbe97e93a6c048363bd2b5dd6
Hoshimikan6490 <creative6490@gmail.com>
2023-06-27 16:22:00 +0900

    ダブルコーテーションに変更

--- commands/birthday_register.js
+++ commands/birthday_register.js
@@ -1,32 +1,32 @@
-const { ApplicationCommandOptionType } = require('discord.js');
-const profileModel = require('../models/profileSchema.js');
+const { ApplicationCommandOptionType } = require("discord.js");
+const profileModel = require("../models/profileSchema.js");
 
 module.exports = {
   data: {
-    name: 'birthday_register',
-    description: '🔧誕生日を登録・更新しよう！',
+    name: "birthday_register",
+    description: "🔧誕生日を登録・更新しよう！",
     options: [
       {
         type: ApplicationCommandOptionType.Number,
-        name: 'month',
-        description: '誕生月を入力してください（半角数字で「1」~「12」を入力）',
-        value: 'month',
+        name: "month",
+        description: "誕生月を入力してください（半角数字で「1」~「12」を入力）",
+        value: "month",
         required: true,
       },
       {
         type: ApplicationCommandOptionType.Number,
-        name: 'day',
-        description: '誕生日を入力してください(半角数字で「1」~「31」を入力)',
-        value: 'day',
+        name: "day",
+        description: "誕生日を入力してください(半角数字で「1」~「31」を入力)",
+        value: "day",
         required: true,
       },
     ],
   },
   async execute(interaction) {
-    if (interaction.guild.id == '768073209169444884') {
+    if (interaction.guild.id == "768073209169444884") {
       // スラッシュコマンドの入力情報を取得
-      var new_birthday_month = interaction.options.getNumber('month');
-      var new_birthday_day = interaction.options.getNumber('day');
+      var new_birthday_month = interaction.options.getNumber("month");
+      var new_birthday_day = interaction.options.getNumber("day");
       let lastday = new Date(2020, new_birthday_month, 0).getDate();
 
       let user_id = interaction.user.id;
@@ -34,10 +34,10 @@ module.exports = {
       if (new_birthday_month >= 1 && new_birthday_month <= 12) {
         if (new_birthday_day >= 1 && new_birthday_day <= lastday) {
           if (new_birthday_month >= 1 && new_birthday_month <= 9) {
-            var new_birthday_month = '0' + new_birthday_month;
+            var new_birthday_month = "0" + new_birthday_month;
           }
           if (new_birthday_day >= 1 && new_birthday_day <= 9) {
-            var new_birthday_day = '0' + new_birthday_day;
+            var new_birthday_day = "0" + new_birthday_day;
           }
           let database_data = await profileModel.findById(user_id);
           let database_month = database_data.birthday_month;
@@ -46,8 +46,8 @@ module.exports = {
             `---データベースからのデータ---\nmonth: ${database_month}\nday: ${database_day}\n------`
           );
 
-          if (database_month == 'no_data') {
-            if (database_day == 'no_data') {
+          if (database_month == "no_data") {
+            if (database_day == "no_data") {
               profileModel.findOne({ _id: user_id }, function (err, model) {
                 if (err) {
                   console.log(err.message);
@@ -57,19 +57,19 @@ module.exports = {
                 // 内容を更新
                 model.birthday_month = new_birthday_month;
                 model.birthday_day = new_birthday_day;
-                model.status = 'yet';
+                model.status = "yet";
                 model.save(async function (err, model) {
                   if (err) {
                     console.log(err.message);
                     await interaction.reply(
-                      '申し訳ございません。内部エラーが発生しました。\n開発者(<@728495196303523900>)が対応しますので、しばらくお待ちください。\n\n----業務連絡---\nデータベースの更新時にエラーが発生しました。\nコンソールを確認してください。'
+                      "申し訳ございません。内部エラーが発生しました。\n開発者(<@728495196303523900>)が対応しますので、しばらくお待ちください。\n\n----業務連絡---\nデータベースの更新時にエラーが発生しました。\nコンソールを確認してください。"
                     );
                     return;
                   } else {
                     await interaction.reply({
                       embeds: [
                         {
-                          title: '新規登録完了！',
+                          title: "新規登録完了！",
                           description: `あなたの誕生日を\`${new_birthday_month}月${new_birthday_day}日\`に設定しました。`,
                           color: 0x0000ff,
                         },
@@ -81,13 +81,13 @@ module.exports = {
               });
             } else {
               await interaction.reply(
-                '申し訳ございません。内部エラーが発生しました。\n開発者(<@728495196303523900>)が対応しますので、しばらくお待ちください。\n\n----業務連絡---\nデータベースのmonthだけがno_dataでした。'
+                "申し訳ございません。内部エラーが発生しました。\n開発者(<@728495196303523900>)が対応しますので、しばらくお待ちください。\n\n----業務連絡---\nデータベースのmonthだけがno_dataでした。"
               );
             }
           } else {
-            if (database_day == 'no_data') {
+            if (database_day == "no_data") {
               await interaction.reply(
-                '申し訳ございません。内部エラーが発生しました。\n開発者(<@728495196303523900>)が対応しますので、しばらくお待ちください。\n\n----業務連絡---\nデータベースのdayだけがno_dataでした。'
+                "申し訳ございません。内部エラーが発生しました。\n開発者(<@728495196303523900>)が対応しますので、しばらくお待ちください。\n\n----業務連絡---\nデータベースのdayだけがno_dataでした。"
               );
             } else {
               profileModel.findOne({ _id: user_id }).then((model) => {
@@ -97,27 +97,26 @@ module.exports = {
                 // 内容を更新
                 model.birthday_month = new_birthday_month;
                 model.birthday_day = new_birthday_day;
-                model.save()
-                  .then(async () => {
-                    await interaction.reply({
-                      embeds: [
-                        {
-                          title: '更新完了！',
-                          description: `あなたの誕生日を\`${old_month}月${old_day}日\`から\`${new_birthday_month}月${new_birthday_day}日\`に更新しました。`,
-                          color: 0x10ff00,
-                        },
-                      ],
-                    });
-                    return;
+                model.save().then(async () => {
+                  await interaction.reply({
+                    embeds: [
+                      {
+                        title: "更新完了！",
+                        description: `あなたの誕生日を\`${old_month}月${old_day}日\`から\`${new_birthday_month}月${new_birthday_day}日\`に更新しました。`,
+                        color: 0x10ff00,
+                      },
+                    ],
                   });
+                  return;
+                });
               });
-            };
+            }
           }
         } else {
           await interaction.reply({
             embeds: [
               {
-                title: 'エラー！',
+                title: "エラー！",
                 description: `${new_birthday_month}月には、最大で${lastday}日までしか存在しません。\n正しい月日使用して再度お試しください。`,
                 color: 0xff0000,
               },
@@ -129,7 +128,7 @@ module.exports = {
         await interaction.reply({
           embeds: [
             {
-              title: 'エラー！',
+              title: "エラー！",
               description: `1年は1～12月までしか存在しません。\n正しい月日を使用して再度お試しください。`,
               color: 0xff0000,
             },
@@ -141,7 +140,7 @@ module.exports = {
       await interaction.reply({
         embeds: [
           {
-            title: 'エラー！',
+            title: "エラー！",
             description: `このサーバーでこのコマンドは実行できません。`,
             color: 0xff0000,
           },
@@ -149,5 +148,5 @@ module.exports = {
         ephemeral: true,
       });
     }
-  }
-}
+  },
+};
