7d033673fca29c444a3bb189f44a189335dd054c
Hoshimikan6490 <creative6490@gmail.com>
2023-08-12 19:25:13 +0900

    エラー対策

--- commands/grade_role_update.js
+++ commands/grade_role_update.js
@@ -1,24 +1,81 @@
 const { PermissionsBitField } = require("discord.js");
 const { setTimeout } = require("node:timers/promises");
+const { client } = require("../index.js");
 
-//TODO => BOTにそれらのロールを編集する権限が無い時（順番がおかしい場合も含む）に、エラーを吐くように変更
 module.exports = {
   data: {
     name: "grade_role_update",
     description: "⬆学年ロールを更新します(サーバー管理者限定)",
   },
   async execute(interaction) {
+    //スラッシュコマンドを実行した人の権限チェック
     if (
       !interaction.member.permissions.has(
         PermissionsBitField.Flags.Administrator
       )
     ) {
+      //エラーを投げる
       interaction.reply({
         content: "申し訳ございません。\nこのコマンドは管理者限定です",
         ephemeral: true,
       });
     } else {
+      //時間かかるので、先にreply
       await interaction.deferReply();
+      let roles = [
+        "高校3年生",
+        "高校2年生",
+        "高校1年生",
+        "中学3年生",
+        "中学2年生",
+        "中学1年生",
+        "生徒",
+        "卒業生",
+      ];
+      let guildMe = await interaction.guild.members.fetch(client.user.id);
+
+      //ロールがあるかチェック
+      for (const key in roles) {
+        let role = await interaction.guild.roles.cache.find((role) =>
+          role.name.includes(roles[key])
+        );
+        if (!role) {
+          return interaction.editReply({
+            embeds: [
+              {
+                title: ":warning: エラー！",
+                description:
+                  "更新で使用するロールが存在しないため、ロールを更新できません。", //TODO => どんなロールが必要か具体的に記述
+                color: 0xff0000,
+                timestamp: new Date(),
+              },
+            ],
+          });
+        }
+      }
+
+      //botの権限チェック
+      for (const key in roles) {
+        let otherRole = await interaction.guild.roles.cache.find((role) =>
+          role.name.includes(roles[key])
+        );
+        let compare = guildMe.roles.highest.comparePositionTo(otherRole);
+        if (compare < 0) {
+          return interaction.editReply({
+            embeds: [
+              {
+                title: ":warning: エラー！",
+                description:
+                  "私に割り当てられている最高順位のロールよりも、更新するロールの位置の方が高いため、ロールを更新できません。私に割り当てられてるロールのうちの１つ以上を学年用ロールよりも上に設定して、再度実行してください。",
+                color: 0xff0000,
+                timestamp: new Date(),
+              },
+            ],
+          });
+        }
+      }
+
+      //ロール更新
       let grade_role_names = [
         "高校3年生",
         "高校2年生",
@@ -27,17 +84,11 @@ module.exports = {
         "中学2年生",
         "中学1年生",
       ];
-
-      // サーバー内の全メンバーを取得する
       const members = await interaction.guild.members.fetch();
-      // mapを使って全メンバーのユーザータグの配列を作る
       const tags = members.map((member) => member.user.id);
-
       for (var key in tags) {
         const user_id = tags[key];
         const user = interaction.guild.members.cache.get(user_id);
-
-        // 高３だけ別処理
         const grade_role_kou3 = await interaction.guild.roles.cache.find(
           (role) => role.name.includes("高校3年生")
         );
@@ -48,11 +99,11 @@ module.exports = {
           (role) => role.name.includes("生徒")
         );
         if (user.roles.cache.has(grade_role_kou3.id)) {
+          // 高３だけ別処理
           user.roles.remove(grade_role_kou3);
           user.roles.add(grade_role_sotugyo);
           user.roles.remove(grade_role_student);
         } else {
-          ///////////////////////////////////////////////////////////////////
           // それ以外のロール処理
           for (var key2 in grade_role_names) {
             if (Number(key2) !== 0) {
@@ -60,11 +111,11 @@ module.exports = {
               const grade_role_new_name = grade_role_names[Number(key2) - 1];
 
               const grade_role = await interaction.guild.roles.cache.find(
-                (role) => role.name === grade_role_name
+                (role) => role.name.includes(grade_role_name)
               );
               const grade_role_id = grade_role.id;
               const grade_role_new = await interaction.guild.roles.cache.find(
-                (role) => role.name === grade_role_new_name
+                (role) => role.name.includes(grade_role_new_name)
               );
 
               if (user.roles.cache.has(grade_role_id)) {
