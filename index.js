ce47cd9621c1087266bf5c8a9047a24b0a202e0f
Hoshimikan6490 <creative6490@gmail.com>
2023-11-07 11:25:22 +0900

    さっきのの修正版

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const commandFiles = fs
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  console.log(`コマンドの読み込みが完了: ${command.name}`);
+  console.log(`コマンドの読み込みが完了: ${command.data.name}`);
   commands[command.data.name] = command;
 }
 
