b6e9d2e06cdfb2a130115b8a7331fbd2a80c26a4
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-01-15 11:31:21 +0900

    要らんコードの削除

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ require("dotenv").config();
 //機密情報取得
 const token = process.env["bot_token"];
 const mong_db_info = process.env["mongodb_token"];
-const prefix = "mc!";
 const PORT = 8000;
 
 //サイト立ち上げ
