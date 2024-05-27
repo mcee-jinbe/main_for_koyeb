f0c934fc2653ec88fabfb4650e2453a5ca5559a2
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-05-27 23:37:46 +0900

    バグ対策のため、計30文字までの制限を追加

--- commands/1000choen.js
+++ commands/1000choen.js
@@ -14,7 +14,7 @@ module.exports = {
   async execute(interaction) {
     const modal = new ModalBuilder()
       .setCustomId("1000choyen")
-      .setTitle("1000兆円画像生成");
+      .setTitle("1000兆円画像生成(合計30文字以内で入力してください)");
 
     const topInput = new TextInputBuilder()
       .setCustomId("topInput")
@@ -38,6 +38,13 @@ module.exports = {
       .then(async (mInteraction) => {
         const top = mInteraction.fields.getTextInputValue("topInput");
         const bottom = mInteraction.fields.getTextInputValue("bottomInput");
+
+        if (top.length + bottom.length > 30)
+          return mInteraction.reply({
+            content: `上側と下側の合計で30文字以内で入力してください。\n\nあなたが入力した文字列は以下の通りでした。\n\`\`\`\n- 上側：　${top}\n- 下側：　${bottom}\n\`\`\``,
+            ephemeral: true,
+          });
+
         mInteraction.reply({
           embeds: [
             {
@@ -50,6 +57,8 @@ module.exports = {
           ],
         });
       })
-      .catch(console.error);
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
