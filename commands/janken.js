2cba9e723c5e6477f96413937597485c5ba572e0
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-10-09 13:52:13 +0900

    remove secret option

--- commands/janken.js
+++ commands/janken.js
@@ -1,64 +1,163 @@
-const {
-  ApplicationCommandOptionType,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-} = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
   name: "janken",
   description: "✊✌️🖐️じゃんけんをしよう！！",
-  options: [
-    {
-      type: ApplicationCommandOptionType.String,
-      name: "secret",
-      description: "結果を非公開で送信したい場合は設定してください。",
-      required: false,
-      choices: [{ name: "非公開にする", value: "true" }],
-    },
-  ],
 
   run: async (client, interaction) => {
     try {
-      var secret = interaction.options.getString("secret");
-
-      // String => Boolean
-      var secret = secret == "true";
-
       const janken_choice = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
-          .setCustomId(secret ? "secret_pa" : "pa")
+          .setCustomId("pa")
           .setLabel("パー")
           .setStyle(ButtonStyle.Primary)
           .setEmoji("🖐"),
         new ButtonBuilder()
-          .setCustomId(secret ? "secret_cho" : "cho")
+          .setCustomId("cho")
           .setLabel("チョキ")
           .setStyle(ButtonStyle.Success)
           .setEmoji("✌"),
         new ButtonBuilder()
-          .setCustomId(secret ? "secret_gu" : "gu") //TODO　　この後のInteractionCreateの処理を書く。また、omikujiも同じ事する
+          .setCustomId("gu")
           .setLabel("グー")
           .setStyle(ButtonStyle.Danger)
           .setEmoji("✊")
       );
-      await interaction.reply({
-        embeds: [
-          {
-            title: "↓何を出す？！↓",
-            color: 0xff0000,
-            thumbnail: {
-              url: "attachment://file.png",
+
+      await interaction
+        .reply({
+          embeds: [
+            {
+              title: "↓何を出す？！↓",
+              color: 0xff0000,
+              thumbnail: {
+                url: "attachment://file.png",
+              },
+              footer: {
+                text: `画像：　じゃんけんのイラスト｜ツカッテ`,
+              },
             },
-            footer: {
-              text: `画像：　じゃんけんのイラスト｜ツカッテ`,
-            },
-          },
-        ],
-        files: [{ attachment: "images/janken.png", name: "file.png" }],
-        components: [janken_choice],
-        ephemeral: true,
-      });
+          ],
+          files: [{ attachment: "images/janken.png", name: "file.png" }],
+          components: [janken_choice],
+          ephemeral: secret,
+        })
+        .then((buttonMessage) => {
+          const filter = (i) => i.user.id == interaction.user.id;
+          const collector = buttonMessage.createMessageComponentCollector({
+            filter,
+            time: 120000,
+          });
+
+          collector.on("collect", async (button) => {
+            let buttonId = button?.customId;
+            if (buttonId == "gu" || buttonId == "cho" || buttonId == "pa") {
+              // じんべえの手を決める
+              const arr = ["pa", "cho", "gu"];
+              const random = Math.floor(Math.random() * arr.length);
+              const jinbe = arr[random];
+              // 自分の手を「me」に代入
+              if (buttonId.includes("pa")) {
+                var me = "pa";
+              } else if (buttonId.includes("cho")) {
+                var me = "cho";
+              } else if (buttonId.includes("gu")) {
+                var me = "gu";
+              }
+              // 結果判定
+              // 自分がパーの時
+              if (buttonId.includes("pa")) {
+                if (jinbe === "pa") {
+                  var jan_result = "aiko";
+                } else if (jinbe === "cho") {
+                  var jan_result = "lose";
+                } else if (jinbe === "gu") {
+                  var jan_result = "win";
+                }
+                // 自分がチョキの時
+              } else if (buttonId.includes("cho")) {
+                if (jinbe === "pa") {
+                  var jan_result = "win";
+                } else if (jinbe === "cho") {
+                  var jan_result = "aiko";
+                } else if (jinbe === "gu") {
+                  var jan_result = "lose";
+                }
+              } else if (buttonId.includes("gu")) {
+                // 自分がグーの時
+                if (jinbe === "pa") {
+                  var jan_result = "lose";
+                } else if (jinbe === "cho") {
+                  var jan_result = "win";
+                } else if (jinbe === "gu") {
+                  var jan_result = "aiko";
+                }
+              }
+              // 変数調整
+              //me変数の日本語化
+              if (me === "pa") {
+                var result_me = "パー";
+              } else if (me === "cho") {
+                var result_me = "チョキ";
+              } else if (me === "gu") {
+                var result_me = "グー";
+              }
+              //jinbe変数の日本語化
+              if (jinbe === "pa") {
+                var result_jinbe = "パー";
+              } else if (jinbe === "cho") {
+                var result_jinbe = "チョキ";
+              } else if (jinbe === "gu") {
+                var result_jinbe = "グー";
+              }
+              //結果の日本語化
+              if (jan_result === "win") {
+                var result_ja = "あなたの勝ち";
+              } else if (jan_result === "aiko") {
+                var result_ja = "あいこ";
+              } else if (jan_result === "lose") {
+                var result_ja = "あなたの負け";
+              }
+              // 色調整
+              if (jan_result === "win") {
+                var color = 0xff0000;
+              } else if (jan_result === "aiko") {
+                var color = 0xffff00;
+              } else if (jan_result === "lose") {
+                var color = 0x0000ff;
+              }
+              // file_pass設定
+              if (jan_result === "win") {
+                var file_pas = "images/win.png";
+              } else if (jan_result === "aiko") {
+                var file_pas = "images/aiko.png";
+              } else if (jan_result === "lose") {
+                var file_pas = "images/lose.png";
+              }
+
+              // 結果表示
+              await interaction.editReply({
+                embeds: [
+                  {
+                    title: "じゃんけんの結果！",
+                    description: `<@${interaction.user.id}>さんは ${result_me}を出して、\n私は　${result_jinbe}を出したので、\n\n__**${result_ja}です！**__`,
+                    color: color,
+                    thumbnail: {
+                      url: "attachment://omi_kekka.png",
+                    },
+                  },
+                ],
+                files: [{ attachment: file_pas, name: "omi_kekka.png" }],
+                components: [],
+              });
+            }
+          });
+        })
+        .catch((err) => {
+          err.id = "janken_collection";
+          const errorNotification = require("../errorFunction.js");
+          errorNotification(client, interaction, err);
+        });
     } catch (err) {
       const errorNotification = require("../errorFunction.js");
       errorNotification(client, interaction, err);
