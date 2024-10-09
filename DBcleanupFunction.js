315f172e46a021242665e51ff7d9c2340b236ea1
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-10-09 17:31:51 +0900

    add error notification

--- DBcleanupFunction.js
+++ DBcleanupFunction.js
@@ -2,28 +2,34 @@ async function deleteUserDBWithoutServerDB() {
   const userDB = require("./models/user_db.js");
   const serverDB = require("./models/server_db.js");
 
-  let users = await userDB.find();
+  try {
+    let users = await userDB.find();
 
-  for (const key of users) {
-    await serverDB
-      .findById(key.serverID)
-      .catch((err) => {
-        console.log(err);
-      })
-      .then((model) => {
-        if (!model) {
-          userDB
-            .deleteOne({ uid: key.uid, serverID: key.serverID })
-            .catch((err) => {
-              console.log(err);
-            })
-            .then(() => {
-              console.log(
-                `正常にサーバーID「${key.serverID}」のデータを削除しました`
-              );
-            });
-        }
-      });
+    for (const key of users) {
+      await serverDB
+        .findById(key.serverID)
+        .catch((err) => {
+          console.log(err);
+        })
+        .then((model) => {
+          if (!model) {
+            userDB
+              .deleteOne({ uid: key.uid, serverID: key.serverID })
+              .catch((err) => {
+                console.log(err);
+              })
+              .then(() => {
+                console.log(
+                  `正常にサーバーID「${key.serverID}」のデータを削除しました`
+                );
+              });
+          }
+        });
+    }
+  } catch (err) {
+    err.id = "DBcleanupFunction";
+    const errorNotification = require("../errorFunction.js");
+    errorNotification(client, interaction, err);
   }
 }
 
