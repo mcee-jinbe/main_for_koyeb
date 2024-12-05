fd9a742283e2616af5722f4595c242b7c9dd09bd
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-12-06 08:34:20 +0900

    add security system

--- commands/happy_birthday.js
+++ commands/happy_birthday.js
@@ -1,4 +1,5 @@
 const { ApplicationCommandOptionType } = require("discord.js");
+const fs = require("fs");
 
 module.exports = {
   name: "happy_birthday",
@@ -16,19 +17,48 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const user = interaction.options.getUser("user");
-      await interaction.reply({
-        content: `<@${user.id}>`,
-        embeds: [
-          {
-            title: "🎊たんおめ！🎊",
-            description:
-              `<@${user.id}>さん　お誕生日おめでとうございます！`,
-            color: 0xff30ff,
-            timestamp: new Date(),
-          },
-        ],
-      });
+      let happyBirthday_sentUser = fs.readFileSync(
+        "./happyBirthday_sentUser.json"
+      );
+      happyBirthday_sentUser = JSON.parse(happyBirthday_sentUser);
+
+      if (!happyBirthday_sentUser.userId.includes(interaction.user.id)) {
+        // 1分間使えなくなるようにする
+        happyBirthday_sentUser.userId.push(interaction.user.id);
+        fs.writeFileSync(
+          "./happyBirthday_sentUser.json",
+          JSON.stringify(happyBirthday_sentUser)
+        );
+
+        const user = interaction.options.getUser("user");
+        await interaction.reply({
+          content: `<@${user.id}>`,
+          embeds: [
+            {
+              title: "🎊たんおめ！🎊",
+              description: `<@${user.id}>さん　お誕生日おめでとうございます！`,
+              color: 0xff30ff,
+              timestamp: new Date(),
+            },
+          ],
+        });
+
+        //1分後にデータ削除して、再度コマンドを実行できるようにする
+        setTimeout(() => {
+          happyBirthday_sentUser.userId = happyBirthday_sentUser.userId.filter(
+            (data) => data != interaction.user.id
+          );
+          fs.writeFileSync(
+            "./happyBirthday_sentUser.json",
+            JSON.stringify(happyBirthday_sentUser)
+          );
+        }, 60000);
+      } else {
+        return interaction.reply({
+          content: `申し訳ございません。本コマンドはスパム対策のため、コマンド実行後一定時間このコマンドは使用できません。少し待ってもう一度お試しください。`,
+          ephemeral: true,
+        });
+      }
     } catch (err) {
       err.id = "happy_birthday";
       const errorNotification = require("../errorFunction.js");
